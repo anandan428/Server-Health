2630db3eb78ddace396f968d99a3be84cfdccd2d
A Nandan <nandan.a@volvo.com>
2018-11-24 07:00:27 +0530

    removed node-sspi for sockets connection, will add it back soon

--- api/modules/user/routes/user.route.js
+++ api/modules/user/routes/user.route.js
@@ -1,14 +1,16 @@
 module.exports = (app, { User }) => {
     var userController = require('../controller/user.controller')(User);
-    app.use((req, res, next) => {
-        var nodeSSPI = require('node-sspi');
-        var nodeSSPIObj = new nodeSSPI({
-            retrieveGroups: true
-        })
-        nodeSSPIObj.authenticate(req, res, function (err) {
-            res.finished || next()
-        })
-    }).route('/api/User')
+    // app.use((req, res, next) => {
+    //     var nodeSSPI = require('node-sspi');
+    //     var nodeSSPIObj = new nodeSSPI({
+    //         retrieveGroups: true
+    //     })
+    //     nodeSSPIObj.authenticate(req, res, function (err) {
+    //         res.finished || next()
+    //     })
+    // })
+    //     "node-sspi": "^0.2.3",
+    app.route('/api/User')
         .get(userController.getUser)
         .post(userController.create);
 }
